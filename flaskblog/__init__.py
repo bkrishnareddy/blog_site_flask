65b2f26511bd74dd22cd618f533e95f572f5536e
B Krishna Reddy <mr.bkrishnareddy@gmail.com>
2018-11-11 16:03:56 +0530

    First Commit

---
+++ flaskblog/__init__.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+
+
+app.config['SECRET_KEY'] = 'cb88bf22d1626bb0fc88bd4f76ffd91e'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
+
+from flaskblog import routs
+
